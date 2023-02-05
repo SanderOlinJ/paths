a251aba0f22066290153f87184fbcb9f2d14d384
Sander Johansen <sander.olin@gmail.com>
2023-02-05 16:12:57 +0100

    Added Passage tests

---
+++ src/test/java/edu/ntnu/idatt2001/sojohans/domain/PassageTest.java
@@ -0,0 +1,37 @@
+package edu.ntnu.idatt2001.sojohans.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassageTest {
+
+    @Test
+    void testThatConstructorHandlesExceptions(){
+        String title = null;
+        String content = "Empty";
+
+        assertThrows(IllegalArgumentException.class, () ->
+                new Passage(title, content));
+    }
+    @Test
+    void testThatLinksAreAddedToPassage(){
+        Passage passage = new Passage("Title", "Content");
+        Link link = new Link("Text", "Reference");
+        passage.addLink(link);
+
+        List<Link> copyOfLinks = passage.getLinks();
+        assertTrue(copyOfLinks.contains(link));
+    }
+
+    @Test
+    void testThatHasUnitsReturnsCorrectly(){
+        Passage passage = new Passage("Title", "Content");
+        Link link = new Link("Text", "Reference");
+        passage.addLink(link);
+
+        assertTrue(passage.hasLinks());
+    }
+}
