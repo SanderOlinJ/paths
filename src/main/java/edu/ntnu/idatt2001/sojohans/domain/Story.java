d5bbbdbece72045505ff1a04dc5bb3768fb110e6
Sander Johansen <sander.olin@gmail.com>
2023-02-05 16:24:12 +0100

    Implemented Story

---
+++ src/main/java/edu/ntnu/idatt2001/sojohans/domain/Story.java
@@ -0,0 +1,46 @@
+package edu.ntnu.idatt2001.sojohans.domain;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Story {
+    private final String title;
+    private final Map<Link, Passage> passages;
+    private final Passage openingPassage;
+
+    public Story(String title, Passage openingPassage){
+        if (title == null || openingPassage == null){
+            throw new IllegalArgumentException("Title or openingPassage cannot be null!");
+        }
+        this.title = title;
+        this.openingPassage = openingPassage;
+        this.passages = new HashMap<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Passage getOpeningPassage() {
+        return openingPassage;
+    }
+
+    public void addPassage(Passage passage){
+        if (passage == null){
+            throw new IllegalArgumentException("Passage cannot be null!");
+        }
+        passages.put(new Link(passage.getTitle(), passage.getTitle()), passage);
+    }
+
+    public Passage getPassage(Link link){
+        if (link == null){
+            throw new IllegalArgumentException("Link cannot be null");
+        }
+        return passages.get(link);
+    }
+
+    public Collection<Passage> getPassages() {
+        return passages.values();
+    }
+}
