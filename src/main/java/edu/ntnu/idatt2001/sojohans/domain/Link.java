e16ef7e45f6cb0512de5b1c7a07e43b1cdeca193
Sander Johansen <sander.olin@gmail.com>
2023-02-05 15:21:37 +0100

    Implemented Link

---
+++ src/main/java/edu/ntnu/idatt2001/sojohans/domain/Link.java
@@ -0,0 +1,57 @@
+package edu.ntnu.idatt2001.sojohans.domain;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Link {
+    private String text;
+    private String reference;
+    private List<Action> actions;
+
+    public Link(String text, String reference){
+        this.text = text;
+        this.reference = reference;
+        this.actions = new ArrayList<>();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void addAction(Action action){
+        actions.add(action);
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{ \n" +
+                "text= '" + text + '\'' +
+                ", reference= '" + reference + '\'' +
+                ", actions= " + actions +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Link link = (Link) o;
+        return Objects.equals(text, link.text) && Objects.equals(reference, link.reference)
+                && Objects.equals(actions, link.actions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(text, reference, actions);
+    }
+}
